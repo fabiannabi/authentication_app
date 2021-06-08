b13d973a9dafc331b71d73e1374fb57cd095a5ab
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-08 17:28:13 -0500

    Creates Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     # Communicate with the session_store to the cookie
     if user
-      session['user_id'] = user.id
+      session[:user_id] = user.id
       render json: { status: :created, logged_in: true, user: user }
     else
       render json: { status: 401 }
