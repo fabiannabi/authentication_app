c52278503fadf5bf59ef0e4bc63430a803f9fefe
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-08 17:27:49 -0500

    Adds skip confing for API request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # CSRF token generetad from the sistem but with API,  you need to skip it
+  skip_before_action :verify_authenticity_token
+
 end
